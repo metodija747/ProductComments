66f50a8cd43800d824566a4a011229239352f048
Metodija747 <metodijabucevski@yahoo.com>
2023-07-30 19:07:37 +0200

    Added rest client functionality

--- src/main/java/CommentResource.java
+++ src/main/java/CommentResource.java
@@ -85,11 +85,11 @@ public class CommentResource {
     @Timed(name = "getProductCommentsTime", description = "Time taken to fetch product comments")
     @Metered(name = "getProductCommentsMetered", description = "Rate of getProductComments calls")
     @ConcurrentGauge(name = "getProductCommentsConcurrent", description = "Concurrent getProductComments calls")
-    @Timeout(value = 20, unit = ChronoUnit.SECONDS) // Timeout after 20 seconds
-    @Retry(maxRetries = 3) // Retry up to 3 times
-    @Fallback(fallbackMethod = "getProductCommentsFallback") // Fallback method if all retries fail
-    @CircuitBreaker(requestVolumeThreshold = 4) // Use circuit breaker after 4 failed requests
-    @Bulkhead(5) // Limit concurrent calls to 5
+//    @Timeout(value = 20, unit = ChronoUnit.SECONDS) // Timeout after 20 seconds
+//    @Retry(maxRetries = 3) // Retry up to 3 times
+//    @Fallback(fallbackMethod = "getProductCommentsFallback") // Fallback method if all retries fail
+//    @CircuitBreaker(requestVolumeThreshold = 4) // Use circuit breaker after 4 failed requests
+//    @Bulkhead(5) // Limit concurrent calls to 5
     @Traced
     public Response getProductComments(@PathParam("productId") String productId,
                                        @QueryParam("page") Integer page,
@@ -147,135 +147,135 @@ public class CommentResource {
             span.finish();
         }
     }
-    public Response getProductCommentsFallback(@PathParam("productId") String productId,
-                                               @QueryParam("page") Integer page,
-                                               @QueryParam("pageSize") Integer pageSize) {
-        LOGGER.info("Fallback activated: Unable to fetch product comments at the moment for productId: " + productId);
-        Map<String, String> response = new HashMap<>();
-        response.put("description", "Unable to fetch product comments at the moment. Please try again later.");
-        return Response.ok(response).build();
-    }
+//    public Response getProductCommentsFallback(@PathParam("productId") String productId,
+//                                               @QueryParam("page") Integer page,
+//                                               @QueryParam("pageSize") Integer pageSize) {
+//        LOGGER.info("Fallback activated: Unable to fetch product comments at the moment for productId: " + productId);
+//        Map<String, String> response = new HashMap<>();
+//        response.put("description", "Unable to fetch product comments at the moment. Please try again later.");
+//        return Response.ok(response).build();
+//    }
 
 
-    @POST
-    @Path("/{productId}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Counted(name = "addCommentAndRatingCount", description = "Count of addCommentAndRating calls")
-    @Timed(name = "addCommentAndRatingTime", description = "Time taken to add comment and rating")
-    @Metered(name = "addCommentAndRatingMetered", description = "Rate of addCommentAndRating calls")
-    @ConcurrentGauge(name = "addCommentAndRatingConcurrent", description = "Concurrent addCommentAndRating calls")
-    @Timeout(value = 20, unit = ChronoUnit.SECONDS) // Timeout after 20 seconds
-    @Retry(maxRetries = 3) // Retry up to 3 times
-    @Fallback(fallbackMethod = "addCommentAndRatingFallback") // Fallback method if all retries fail
-    @CircuitBreaker(requestVolumeThreshold = 4) // Use circuit breaker after 4 failed requests
-    @Bulkhead(5) // Limit concurrent calls to 5
-    @Traced
-    public Response addCommentAndRating(@PathParam("productId") String productId,
-                                        CommentRating commentRating) {
-        Span span = tracer.buildSpan("addCommentAndRating").start();
-        span.setTag("productId", productId);
-        Map<String, Object> logMap = new HashMap<>();
-        logMap.put("event", "addCommentAndRating");
-        logMap.put("value", productId);
-        logMap.put("comment", commentRating.getComment());
-        logMap.put("rating", commentRating.getRating());
-        span.log(logMap);
-        checkAndUpdateDynamoDbClient();
-        LOGGER.info("addCommentAndRating method called");
-        if (jwt == null) {
-            LOGGER.info("Unauthorized: only authenticated users can add/update rating to product.");
-            return Response.ok("Unauthorized: only authenticated users can add/update rating to product.").build();
-        }
-        String userId = optSubject.getValue().orElse("default_value");
-        try {
-            Map<String, AttributeValue> attributeValues = new HashMap<>();
-            attributeValues.put(":v_pid", AttributeValue.builder().s(productId).build());
-            attributeValues.put(":v_uid", AttributeValue.builder().s(userId).build());
-
-            QueryRequest userCommentCheckRequest = QueryRequest.builder()
-                    .tableName(currentTableName)
-                    .keyConditionExpression("productId = :v_pid and UserId = :v_uid")
-                    .expressionAttributeValues(attributeValues)
-                    .build();
-            QueryResponse userCommentCheckResponse = dynamoDB.query(userCommentCheckRequest);
-
-            Map<String, AttributeValue> item = new HashMap<>();
-            item.put("UserId", AttributeValue.builder().s(userId).build());
-            item.put("productId", AttributeValue.builder().s(productId).build());
-            item.put("Comment", AttributeValue.builder().s(commentRating.getComment()).build());
-            item.put("Rating", AttributeValue.builder().n(String.valueOf(commentRating.getRating())).build());
-
-            PutItemRequest putItemRequest = PutItemRequest.builder()
-                    .tableName(currentTableName)
-                    .item(item)
-                    .build();
-
-            dynamoDB.putItem(putItemRequest);
-
-            // After successfully adding the comment and rating, calculate the new average rating
-            QueryRequest queryRequest = QueryRequest.builder()
-                    .tableName(currentTableName)
-                    .keyConditionExpression("productId = :v_id")
-                    .expressionAttributeValues(Collections.singletonMap(":v_id", AttributeValue.builder().s(productId).build()))
-                    .projectionExpression("Rating")
-                    .build();
-            QueryResponse queryResponse = dynamoDB.query(queryRequest);
-            List<Map<String, AttributeValue>> comments = queryResponse.items();
-
-            int totalRating = 0;
-            for (Map<String, AttributeValue> commentItem : comments) {
-                totalRating += Integer.parseInt(commentItem.get("Rating").n());
-            }
-            double avgRating = comments.size() > 0 ? Math.round((double) totalRating / comments.size()) : 0;
-            LOGGER.info("DynamoDB response: " + avgRating);
-            LOGGER.info("DynamoDB response: " + userCommentCheckResponse.items().isEmpty());
-
+//    @POST
+//    @Path("/{productId}")
+//    @Consumes(MediaType.APPLICATION_JSON)
+//    @Counted(name = "addCommentAndRatingCount", description = "Count of addCommentAndRating calls")
+//    @Timed(name = "addCommentAndRatingTime", description = "Time taken to add comment and rating")
+//    @Metered(name = "addCommentAndRatingMetered", description = "Rate of addCommentAndRating calls")
+//    @ConcurrentGauge(name = "addCommentAndRatingConcurrent", description = "Concurrent addCommentAndRating calls")
+//    @Timeout(value = 20, unit = ChronoUnit.SECONDS) // Timeout after 20 seconds
+//    @Retry(maxRetries = 3) // Retry up to 3 times
+//    @Fallback(fallbackMethod = "addCommentAndRatingFallback") // Fallback method if all retries fail
+//    @CircuitBreaker(requestVolumeThreshold = 4) // Use circuit breaker after 4 failed requests
+//    @Bulkhead(5) // Limit concurrent calls to 5
+//    @Traced
+//    public Response addCommentAndRating(@PathParam("productId") String productId,
+//                                        CommentRating commentRating) {
+//        Span span = tracer.buildSpan("addCommentAndRating").start();
+//        span.setTag("productId", productId);
+//        Map<String, Object> logMap = new HashMap<>();
+//        logMap.put("event", "addCommentAndRating");
+//        logMap.put("value", productId);
+//        logMap.put("comment", commentRating.getComment());
+//        logMap.put("rating", commentRating.getRating());
+//        span.log(logMap);
+//        checkAndUpdateDynamoDbClient();
+//        LOGGER.info("addCommentAndRating method called");
+//        if (jwt == null) {
+//            LOGGER.info("Unauthorized: only authenticated users can add/update rating to product.");
+//            return Response.ok("Unauthorized: only authenticated users can add/update rating to product.").build();
+//        }
+//        String userId = optSubject.getValue().orElse("default_value");
+//        try {
+//            Map<String, AttributeValue> attributeValues = new HashMap<>();
+//            attributeValues.put(":v_pid", AttributeValue.builder().s(productId).build());
+//            attributeValues.put(":v_uid", AttributeValue.builder().s(userId).build());
+//
+//            QueryRequest userCommentCheckRequest = QueryRequest.builder()
+//                    .tableName(currentTableName)
+//                    .keyConditionExpression("productId = :v_pid and UserId = :v_uid")
+//                    .expressionAttributeValues(attributeValues)
+//                    .build();
+//            QueryResponse userCommentCheckResponse = dynamoDB.query(userCommentCheckRequest);
+//
+//            Map<String, AttributeValue> item = new HashMap<>();
+//            item.put("UserId", AttributeValue.builder().s(userId).build());
+//            item.put("productId", AttributeValue.builder().s(productId).build());
+//            item.put("Comment", AttributeValue.builder().s(commentRating.getComment()).build());
+//            item.put("Rating", AttributeValue.builder().n(String.valueOf(commentRating.getRating())).build());
+//
+//            PutItemRequest putItemRequest = PutItemRequest.builder()
+//                    .tableName(currentTableName)
+//                    .item(item)
+//                    .build();
+//
+//            dynamoDB.putItem(putItemRequest);
+//
+//            // After successfully adding the comment and rating, calculate the new average rating
+//            QueryRequest queryRequest = QueryRequest.builder()
+//                    .tableName(currentTableName)
+//                    .keyConditionExpression("productId = :v_id")
+//                    .expressionAttributeValues(Collections.singletonMap(":v_id", AttributeValue.builder().s(productId).build()))
+//                    .projectionExpression("Rating")
+//                    .build();
+//            QueryResponse queryResponse = dynamoDB.query(queryRequest);
+//            List<Map<String, AttributeValue>> comments = queryResponse.items();
+//
+//            int totalRating = 0;
+//            for (Map<String, AttributeValue> commentItem : comments) {
+//                totalRating += Integer.parseInt(commentItem.get("Rating").n());
+//            }
+//            double avgRating = comments.size() > 0 ? Math.round((double) totalRating / comments.size()) : 0;
+//            LOGGER.info("DynamoDB response: " + avgRating);
+//            LOGGER.info("DynamoDB response: " + userCommentCheckResponse.items().isEmpty());
+//
+////            if (productCatalogUrl.isPresent()) {
+////                Client client = ClientBuilder.newClient();
+////                WebTarget target;
+////                if (userCommentCheckResponse.items().isEmpty()) {
+////                    target = client.target(productCatalogUrl.get().toString() + "/products/" + productId + "?action=add");
+////                } else {
+////                    target = client.target(productCatalogUrl.get().toString() + "/products/" + productId + "?action=zero");
+////                }
+////                Response response = target.request(MediaType.APPLICATION_JSON)
+////                        .header("Authorization", token)
+////                        .put(Entity.entity(avgRating, MediaType.APPLICATION_JSON));
+////
+////                if (response.getStatus() != Response.Status.OK.getStatusCode()) {
+////                    return Response.status(response.getStatus()).entity(response.readEntity(String.class)).build();
+////                }
+////            }
 //            if (productCatalogUrl.isPresent()) {
-//                Client client = ClientBuilder.newClient();
-//                WebTarget target;
-//                if (userCommentCheckResponse.items().isEmpty()) {
-//                    target = client.target(productCatalogUrl.get().toString() + "/products/" + productId + "?action=add");
-//                } else {
-//                    target = client.target(productCatalogUrl.get().toString() + "/products/" + productId + "?action=zero");
-//                }
-//                Response response = target.request(MediaType.APPLICATION_JSON)
-//                        .header("Authorization", token)
-//                        .put(Entity.entity(avgRating, MediaType.APPLICATION_JSON));
+//                ProductCatalogApi api = RestClientBuilder.newBuilder()
+//                        .baseUrl(new URL(productCatalogUrl.get().toString()))
+//                        .build(ProductCatalogApi.class);
+//
+//                String action = userCommentCheckResponse.items().isEmpty() ? "add" : "zero";
+//                String authHeader = "Bearer " + jwt.getRawToken(); // get the raw JWT token and prepend "Bearer "
+//                Response response = api.updateProductRating(productId, action, authHeader, avgRating);
 //
 //                if (response.getStatus() != Response.Status.OK.getStatusCode()) {
 //                    return Response.status(response.getStatus()).entity(response.readEntity(String.class)).build();
 //                }
 //            }
-            if (productCatalogUrl.isPresent()) {
-                ProductCatalogApi api = RestClientBuilder.newBuilder()
-                        .baseUrl(new URL(productCatalogUrl.get().toString()))
-                        .build(ProductCatalogApi.class);
-
-                String action = userCommentCheckResponse.items().isEmpty() ? "add" : "zero";
-                String authHeader = "Bearer " + jwt.getRawToken(); // get the raw JWT token and prepend "Bearer "
-                Response response = api.updateProductRating(productId, action, authHeader, avgRating);
-
-                if (response.getStatus() != Response.Status.OK.getStatusCode()) {
-                    return Response.status(response.getStatus()).entity(response.readEntity(String.class)).build();
-                }
-            }
-            span.setTag("completed", true);
-            return Response.ok("Comment and rating added successfully").build();
-        } catch (DynamoDbException | MalformedURLException e) {
-            LOGGER.log(Level.SEVERE, "Error while adding comment and rating for product " + productId, e);
-            span.setTag("error", true);
-            throw new WebApplicationException("Error while adding comment and rating. Please try again later.", e, Response.Status.INTERNAL_SERVER_ERROR);
-        } finally {
-            span.finish();
-        }
-    }
-    public Response addCommentAndRatingFallback(@PathParam("productId") String productId,
-                                                CommentRating commentRating) {
-        LOGGER.info("Fallback activated: Unable to add comment and rating at the moment for productId: " + productId);
-        Map<String, String> response = new HashMap<>();
-        response.put("description", "Unable to add comment and rating at the moment. Please try again later.");
-        return Response.ok(response).build();
-    }
+//            span.setTag("completed", true);
+//            return Response.ok("Comment and rating added successfully").build();
+//        } catch (DynamoDbException | MalformedURLException e) {
+//            LOGGER.log(Level.SEVERE, "Error while adding comment and rating for product " + productId, e);
+//            span.setTag("error", true);
+//            throw new WebApplicationException("Error while adding comment and rating. Please try again later.", e, Response.Status.INTERNAL_SERVER_ERROR);
+//        } finally {
+//            span.finish();
+//        }
+//    }
+//    public Response addCommentAndRatingFallback(@PathParam("productId") String productId,
+//                                                CommentRating commentRating) {
+//        LOGGER.info("Fallback activated: Unable to add comment and rating at the moment for productId: " + productId);
+//        Map<String, String> response = new HashMap<>();
+//        response.put("description", "Unable to add comment and rating at the moment. Please try again later.");
+//        return Response.ok(response).build();
+//    }
 
 //    @DELETE
 //    @Path("/{productId}")
